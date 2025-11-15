dc34b5a1fe37603fa75d3b98e4a7c21acbccb08c
Lloyd McKie <lloyd.mckie@ibm.com>
2025-11-15 15:31:17 +0800

    feature: add in react version of github pages

---
+++ ui/src/components/careerdevelopment.tsx
@@ -0,0 +1,10 @@
+import {BlogPost} from './blogpost';
+export function CareerDevelopment() {
+  return (
+    <div>
+      <BlogPost title="Mentoring" link='https://lloyd150337.substack.com/p/mentoring-more-than-just-a-title' description='More than just a title'/>
+      <BlogPost title="Speaking" link='https://lloyd150337.substack.com/p/level-up-your-career-the-critical' description='Level up your career'/>
+      <BlogPost title="Growth Behaviors" link='https://lloyd150337.substack.com/p/unlock-your-potential-embracing-the' description='Unlock your potential'/>
+    </div>
+  )
+}
